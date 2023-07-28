015d401f05f416a59483d1ce67edf1154dce0a99
BrianOtto <brian.otto@zoho.com>
2023-07-27 22:21:27 -0700

    Added the initial code

---
+++ index.js
@@ -0,0 +1,54 @@
+import * as client from './lib/partyline/client.js'
+
+window.user = null
+
+client.create(function(event) {
+    client.ws.send(client.createMessage('', 'account-connect'))
+})
+
+let toastMessages = [].slice.call(document.querySelectorAll('.toast'))
+toastMessages.map(function (message) {
+    return new bootstrap.Toast(message)
+})
+
+let toastTriggers = [].slice.call(document.querySelectorAll('[data-toast="construction"]'))
+toastTriggers.map(function (trigger) {
+    trigger.addEventListener('click', function(event) {
+        bootstrap.Toast.getInstance(document.querySelector('#pl-construction')).show()
+    })
+})
+
+document.querySelector('#pl-toggle').addEventListener('click', function(event) {
+    if (document.querySelector('#pl-body').style.display == 'none') {
+        document.querySelector('#pl-toggle i').classList.remove('fa-caret-up')
+        document.querySelector('#pl-toggle i').classList.add('fa-caret-down')
+        
+        document.querySelector('#pl-body').style.display = ''
+        document.querySelector('#pl-head nav').classList.remove('fixed-bottom')
+    } else {
+        document.querySelector('#pl-toggle i').classList.remove('fa-caret-down')
+        document.querySelector('#pl-toggle i').classList.add('fa-caret-up')
+        
+        document.querySelector('#pl-body').style.display = 'none'
+        document.querySelector('#pl-head nav').classList.add('fixed-bottom')
+    }
+})
+
+document.querySelector('#pl-channel-1').addEventListener('click', function(event) {
+    document.querySelector('#pl-channels').classList.add('d-none')
+    document.querySelector('#pl-chat').classList.remove('d-none')
+})
+
+document.querySelector('#pl-channels-back').addEventListener('click', function(event) {
+    document.querySelector('#pl-chat').classList.add('d-none')
+    document.querySelector('#pl-channels').classList.remove('d-none')
+})
+
+document.querySelector('#pl-chat-window textarea').addEventListener('keydown', function(event) {
+    if (event.keyCode == 13) {
+        client.ws.send(client.createMessage(event.target.value, 'channel-message'))
+        
+        event.target.select()
+        event.preventDefault()
+    }
+})
