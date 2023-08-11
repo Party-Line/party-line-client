197136a11e516dbcf74d7d22c7f4c3e344d16669
BrianOtto <brian.otto@zoho.com>
2023-08-10 23:41:17 -0700

    Some refactoring with communication and cookies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as client from './lib/partyline/client.js'
 
 window.user = null
 
-client.create(function (event) {
+client.create((event) => {
     client.ws.send(client.createMessage('', 'account-connect'))
 })
 
@@ -13,12 +13,12 @@ toastMessages.map(function (message) {
 
 let toastTriggers = [].slice.call(document.querySelectorAll('[data-toast="construction"]'))
 toastTriggers.map(function (trigger) {
-    trigger.addEventListener('click', function (event) {
+    trigger.addEventListener('click', (event) => {
         bootstrap.Toast.getInstance(document.querySelector('#pl-construction')).show()
     })
 })
 
-window.addEventListener('load', function (event) {
+window.addEventListener('load', (event) => {
     // let the extension know the window has loaded
     window.postMessage({ action: 'window-loaded' }, '*')
     
@@ -26,7 +26,21 @@ window.addEventListener('load', function (event) {
     window.postMessage({ action: 'window-cookies' }, '*')
 })
 
-document.querySelector('#pl-minmax').addEventListener('click', function (event) {
+window.addEventListener('ext-window-cookies', (event) => {
+    console.log(event.type, JSON.parse(sessionStorage.getItem('window-cookies')))
+    
+    // TODO: get / create the user account and login to chat
+    
+    // pause for 2 seconds so there is no weird blip when the login is fast
+    setTimeout(function() {
+        document.querySelector('#pl-login').classList.add('d-none')
+        document.querySelector('#pl-head').classList.remove('d-none')
+        document.querySelector('#pl-channels').classList.remove('d-none')
+        document.querySelector('#pl-foot').classList.remove('d-none')
+    }, 2000)
+})
+
+document.querySelector('#pl-minmax').addEventListener('click', (event) => {
     /* hiding the chat area looks strange inside of a window
        and so we toggle / minimize the window instead ...
        
@@ -49,17 +63,17 @@ document.querySelector('#pl-minmax').addEventListener('click', function (event) 
     window.postMessage({ action: 'window-toggle' }, '*')
 })
 
-document.querySelector('#pl-channel-1').addEventListener('click', function (event) {
+document.querySelector('#pl-channel-1').addEventListener('click', (event) => {
     document.querySelector('#pl-channels').classList.add('d-none')
     document.querySelector('#pl-chat').classList.remove('d-none')
 })
 
-document.querySelector('#pl-channels-back').addEventListener('click', function (event) {
+document.querySelector('#pl-channels-back').addEventListener('click', (event) => {
     document.querySelector('#pl-chat').classList.add('d-none')
     document.querySelector('#pl-channels').classList.remove('d-none')
 })
 
-document.querySelector('#pl-chat-window textarea').addEventListener('keydown', function (event) {
+document.querySelector('#pl-chat-window textarea').addEventListener('keydown', (event) => {
     if (event.keyCode == 13) {
         client.ws.send(client.createMessage(event.target.value, 'channel-message'))
         
